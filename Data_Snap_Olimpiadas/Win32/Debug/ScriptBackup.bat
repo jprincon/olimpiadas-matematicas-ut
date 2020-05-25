035813a577fbb6421211fa4cfcde9ddd6cd74181
Julián Andrés Rincón Penagos <jarincon@uniquindio.edu.co>
2020-05-24 23:24:43 -0500

    Configuración archivos innecesarios

--- Data_Snap_Olimpiadas/Win32/Debug/ScriptBackup.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd C:\Program Files\PostgreSQL\9.2\bin
-set BACKUP_FILE=C:\xampp\htdocs\downloads\OlimpiadasUT\Archivos\Olimpiadas.backup
-
-pg_dump -h localhost -p 5432 -U postgres -F c -b -v -f %BACKUP_FILE% olimpiadas
